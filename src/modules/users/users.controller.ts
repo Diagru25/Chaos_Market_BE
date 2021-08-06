1e3e6e14534543c9aa6eb788e3cd81c56a82340e
Diagru25 <diagru2510@gmail.com>
2021-08-06 16:56:12 +0700

    [update] update user controller

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,39 +1,47 @@
-import { Body, Controller, Get, Param, Post, Put, Request, UseGuards } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    Param,
+    Post,
+    Put,
+    Request,
+    UseGuards,
+} from '@nestjs/common';
 import { UsersService } from '../database/services/users.service';
 import { Roles, RolesGuard } from '../auth/guards/roles.guard';
-import {Role} from 'src/configs/roles.enum';
+import { Role } from 'src/configs/roles.enum';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('v1/users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+    constructor(private readonly usersService: UsersService) {}
 
-  @UseGuards(RolesGuard)
-  @Roles(Role.Root, Role.Admin)
-  @Get()
-  async findAll() {
-    return this.usersService.findAll();
-  }
+    @UseGuards(JwtAuthGuard)
+    @Get('/info')
+    async findOne(@Request() req) {
+        console.log(req.user);
+        return this.usersService.findOne(req.user.id);
+    }
+    
+    @UseGuards(RolesGuard)
+    @Roles(Role.Root, Role.Admin)
+    @Get()
+    async findAll() {
+        return this.usersService.findAll();
+    }
 
-  @UseGuards(JwtAuthGuard)
-  @Get('/info')
-  async findOne(@Request() req) {
-    console.log(req.user);
-    return this.usersService.findOne(req.user.id);
-  }
+    @UseGuards(RolesGuard)
+    @Roles(Role.Root)
+    @Post()
+    async create(@Body() data: any) {
+        return this.usersService.create(data);
+    }
 
-  @UseGuards(RolesGuard)
-  @Roles(Role.Root)
-  @Post()
-  async create(@Body() data: any) {
-    return this.usersService.create(data);
-  }
-
-  @UseGuards(RolesGuard)
-  @Roles(Role.Root, Role.Admin, Role.User)
-  @Put(':id')
-  async update(@Body() data: any, @Param('id') id: string, @Request() req) {
-      return this.usersService.update(id, data, req.user)
-  }
-
+    @UseGuards(RolesGuard)
+    @Roles(Role.Root, Role.Admin, Role.User)
+    @Put(':id')
+    async update(@Body() data: any, @Param('id') id: string, @Request() req) {
+        return this.usersService.update(id, data, req.user);
+    }
 }
